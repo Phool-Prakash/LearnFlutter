6200c2a4c757c53c861387ee0f9eb569daf78250
Phool Prakash <phoolprakash62@gmail.com>
2024-07-10 12:59:43 +0530

    flutter package

--- lib/statemanagement/Widgets/materialWidget/materialWidget.dart
+++ lib/statemanagement/Widgets/materialWidget/materialWidget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_learn/customButton/customElevatedButton.dart';
 import 'package:flutter_learn/customButton/customFilledButton.dart';
+import 'package:flutter_package/flutter_package.dart';
 
 class MaterialWidget extends StatefulWidget {
   const MaterialWidget({super.key});
@@ -32,7 +33,24 @@ class _MaterialWidgetState extends State<MaterialWidget> {
               onPressed: () {},
               text: 'Actions',
             )),
-            CustomFilledButton()
+            const CustomFilledButton(),
+            const SizedBox(
+              height: 20,
+            ),
+
+            ///flutter package (own)
+            // const FancyContainersNew(
+            //   width: 350,
+            //   height: 100,
+            //   color1: Colors.white,
+            //   color2: Colors.purple,
+            //   title: 'Saket',
+            //   subtitle: 'Kumar',
+            //   textColor: Colors.black,
+            //   subtitleColor: Colors.black,
+            //   titleTextSize: 25,
+            //   subTitleTextSize: 18,
+            // )
           ],
         ),
       ),
